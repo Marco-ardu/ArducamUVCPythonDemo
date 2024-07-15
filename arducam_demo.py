c33cf1a81775251f126e6b39ff6200f230a9d696
xiaotang <Marco@arducam.com>
2024-07-15 13:06:30 +0800

    remove isp

--- arducam_demo.py
+++ arducam_demo.py
@@ -1,9 +1,7 @@
 import cv2
 import argparse
 
-import numpy as np
 from camera import Camera
-from isp import arducam108mp_isp
 from utils import *
 import json
 from rich import print
@@ -13,14 +11,13 @@ display_fps.frame_count = 0
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-W', '--width', type=int, required=False, default=0, help='set camera image width')
-    parser.add_argument('-H', '--height', type=int, required=False, default=0, help='set camera image height')
+    parser.add_argument('-W', '--width', type=int, required=False, default=1280, help='set camera image width')
+    parser.add_argument('-H', '--height', type=int, required=False, default=720, help='set camera image height')
     parser.add_argument('-d', '--DisplayWindow', type=validate_windows_size, required=False, default="800:600", help='Set the display window size, <width>:<height>')
-    parser.add_argument('-f', '--FrameRate', type=int, required=False, default=0, help='set camera frame rate')
+    parser.add_argument('-f', '--FrameRate', type=int, required=False, default=30, help='set camera frame rate')
     parser.add_argument('-F', '--Focus', action='store_true', required=False, help='Add focus control on the display interface')
     parser.add_argument('-i', '--index', type=int, required=False, default=0, help='set camera index')
     parser.add_argument('-v', '--VideoCaptureAPI', type=int, required=False, default=0, choices=range(0, len(selector_list)), help=VideoCaptureAPIs)
-    parser.add_argument('-o', '--OutputPath', type=str, required=False, help="set save image path")
     parser.add_argument('-t', '--reStartTimes', type=int, required=False, default=5, help="restart camera times")
     parser.add_argument('--ccm', action='store_true', required=False, help="add color correction")
     parser.add_argument('--tuning-file', type=str, required=False, help="tuning file path")
@@ -33,7 +30,6 @@ if __name__ == "__main__":
     index = args.index
     fps = args.FrameRate
     focus = args.Focus
-    output_path = args.OutputPath
     restart_times = args.reStartTimes
     ccm = args.ccm
     tuning_file_path = args.tuning_file
@@ -56,9 +52,6 @@ if __name__ == "__main__":
     
     if focus:
         cv2.createTrackbar('Focus', 'video', 187, 1023, cap.set_focus)
-
-    if width == 6000 and height == 9000:
-        cap.set(cv2.CAP_PROP_CONVERT_RGB, 0)
 
     ccm_list = []
     if tuning_file_path:
@@ -84,33 +77,28 @@ if __name__ == "__main__":
                     continue
                 else:
                     print("reopen failed")
-        
-        if width == 6000 and height == 9000:
-            frame = arducam108mp_isp(frame.reshape(9000, 12000), ccm, ccm_list)
 
         display_fps(frame)
         cv2.imshow("video", frame)
         
-        time_str = time.strftime('%Y-%m-%d') + time.strftime('_%H_%M_%S')
         key = cv2.waitKey(1)                                            
         if key == ord("q"):
             break
         elif key == ord("s"):
-            if not output_path:
-                output_path = f"{width}x{height}_{time_str}.jpg"
+            time_str = time.strftime('%Y-%m-%d') + time.strftime('_%H_%M_%S')
+            output_path = f"{width}x{height}_{time_str}.jpg"
             cv2.imwrite(f"{output_path}", frame)
             print(f"save success, file name: {output_path}")
         elif key == ord("a"):
-            cap.set_width(6000)
+            cap.set_width(12000)
             cap.set_height(9000)
             cap.reStart()
-            cap.set(cv2.CAP_PROP_CONVERT_RGB, 0)
 
             for i in range(wait_frames):
                 print(f"wait {i + 1}")
                 ret, frame = cap.read()
             if ret:
-                frame = arducam108mp_isp(frame.reshape(9000, 12000), ccm, ccm_list)
+                time_str = time.strftime('%Y-%m-%d') + time.strftime('_%H_%M_%S')
                 file_name = f"108MP_{time_str}.jpg"
                 cv2.imwrite(file_name, frame)
                 print(f"save success, file name: {file_name}")
@@ -123,5 +111,4 @@ if __name__ == "__main__":
             cap.reStart()
 
     cap.release()
-
     cv2.destroyAllWindows()
